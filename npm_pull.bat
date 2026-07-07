b824a2ce449eb04e8cb1e9c16bac4ab4bf4f58ac
allenoretaaban@gmail.com <DeusFortunae@77>
2026-07-08 07:56:20 +0800

    update

---
+++ npm_pull.bat
@@ -0,0 +1,10 @@
+@echo off
+title Badong's npm process 
+cd /d "\"
+cd /d "C:\Projects\app"
+
+echo Updating Badong's Inventory System...
+echo Do not close this window while using the application.
+echo ----------------------------------------------------
+
+npm run build
